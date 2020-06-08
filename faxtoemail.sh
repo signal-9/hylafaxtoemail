63f6ac298081c69e02c016dfe89ab83ad05b1846
signal-9 <66137938+signal-9@users.noreply.github.com>
2020-06-08 15:00:08 -0400

    Update faxtoemail.sh

--- faxtoemail.sh
+++ faxtoemail.sh
@@ -13,14 +13,14 @@ SMTPPASS=mysupersecurepassword
 MESSAGEBODY="New Fax Recieved.  Say whatever you want here."
 SUBJECT="New Fax, or something"
 
-# Watch the target directory for new files, wait 30
-# seconds, then convert to pdf and email the file.
+# Watch the target directory for new files, wait 5
+# minutes (about 20 pages worth), then convert to pdf and email the file.
 
 inotifywait -m -e create --format "%f" $TARGET \
         | while read FILENAME
                 do
                         echo Detected $FILENAME, converting to pdf and emailing
-                        sleep 30s
+                        sleep 5m
                         tiff2pdf -o "$PROCESSED/$FILENAME.pdf" "$TARGET/$FILENAME"
                         wait
                         sleep 10s
